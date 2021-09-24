05bfb01d8fd0eb0286ca2b165fb46ab7f89eff94
Carly Morris <thabai@hotmail.com>
2021-09-24 16:50:42 +0100

    testing of album class complete

--- src/test/java/JukeboxTests/PlaylistTest.java
+++ src/test/java/JukeboxTests/PlaylistTest.java
@@ -1,6 +1,23 @@
 package JukeboxTests;
 
-public class PlaylistTest {
+import Jukebox.Song;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlaylistTest {
+//    @Test
+//    public void testGetSetArtist(){
+//        Song testSong = new Song("Testy", "Testy");
+//        testSong.setArtist("Beach Boys");
+//        assertEquals("Beach Boys", testSong.getArtist(),"No Artist");
+//    }
+//
+//    @Test
+//    public void testGetSetSong(){
+//        Song testSong = new Song();
+//        testSong.setSong("Surfin USA", "Beach Boys");
+//        assertEquals("Now playing Surfin USA by the Beach Boys", testSong.getSong(),"No Artist");
+//    }
 
 }
